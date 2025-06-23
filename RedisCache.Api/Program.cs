b133dbb5ced549eb92c4c3e1275da669d4a73e91
Martik Avagyan <avagyanmartik05@gmail.com>
2025-06-24 00:04:11 +0400

    Implementation Redis (#1)

--- RedisCache.Api/Program.cs
+++ RedisCache.Api/Program.cs
@@ -1,8 +1,31 @@
+using StackExchange.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    if (!builder.Environment.IsProduction())
+    {
+        options.Configuration = builder.Configuration["Redis:Endpoint"];
+    }
+    else
+    {
+        options.ConfigurationOptions = new ConfigurationOptions
+        {
+            EndPoints = { builder.Configuration["Redis:Endpoint"]! },
+            Password = builder.Configuration["Redis:Password"],
+            Ssl = true,
+            AbortOnConnectFail = true,
+            AllowAdmin = true,
+        };
+    }
+
+    options.InstanceName = builder.Configuration["Redis:Instance"];
+});
 
 var app = builder.Build();
 
